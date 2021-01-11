8a5d72b427f313deae13651ede58a5425dfd2aba
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-11 06:23:02 +0300

    FInished Practice probls

--- lesson3/practProbHard1.js
+++ lesson3/practProbHard1.js
@@ -1,18 +1,51 @@
-function messWithVars(one, two, three) {
-  one.splice(0, 1, "two");
-  two.splice(0, 1, "three");
-  three.splice(0, 1, "one");
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+
+  if (dotSeparatedWords.length < 4 || dotSeparatedWords.length > 4) {
+    return false;
+  }
+
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+    return true;
 }
 
-let one = ["one"];
-let two = ["two"];
-let three = ["three"];
+function isAnIpNumber(str) {
+  if (/^\d+$/.test(str)) {
+    let number = Number(str);
+    return number >= 0 && number <= 255;
+  }
 
-messWithVars(one, two, three);
+  return false;
+}
 
-console.log(`one is: ${one}`);
-console.log(`two is: ${two}`);
-console.log(`three is: ${three}`);
+
+console.log(isDotSeparatedIpAddress("1.2.3.4.5"));
+console.log(isDotSeparatedIpAddress("4.5.5"));
+console.log(isDotSeparatedIpAddress("4.5.5.4"));
+console.log(isDotSeparatedIpAddress("4.5.5"));
+
+
+// function messWithVars(one, two, three) {
+//   one.splice(0, 1, "two");
+//   two.splice(0, 1, "three");
+//   three.splice(0, 1, "one");
+// }
+
+// let one = ["one"];
+// let two = ["two"];
+// let three = ["three"];
+
+// messWithVars(one, two, three);
+
+// console.log(`one is: ${one}`);
+// console.log(`two is: ${two}`);
+// console.log(`three is: ${three}`);
 
 // function messWithVars(one, two, three) {
 //   one = ["two"];
---
+++ lesson4/collections.js
@@ -0,0 +1,28 @@
+let obj = { apple: 'Produce', carrot: 'Produce', pear: 'Produce', broccoli: 'Produce' }
+obj['apple'] = 'Fruit';
+obj.carrot = 'Vegetable';
+obj["pear"] = "Fruit";
+obj["broccoli"] = "Vegetable";
+
+console.log(obj);
+
+
+// let numbers = [1, 2, 3, 4];
+// // for(let i = 0; i < numbers.length; i++) {
+// numbers[0] = numbers[0] + 1;  // => 2
+// numbers[1] = numbers[1] + 1;  // => 2
+// numbers[2] = numbers[2] + 1;  // => 2
+// numbers[3] = numbers[3] + 1;  // => 2
+// numbers[4] = numbers[4] + 1;  // => 2
+
+// // return numbers;                      // => [ 2, 2, 3, 4 ]
+
+// console.log(numbers);
+
+
+// let obj = { fruit: 'apple', vegetable: 'carrot', fruit: 'pear' }
+
+// console.log(obj);
+// let str = 'The grass is green'
+
+// console.log(str.slice(4,9));

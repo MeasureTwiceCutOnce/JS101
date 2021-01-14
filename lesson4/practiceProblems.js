1642c7f9b7b130c5d48bee0a3fc6582980cd30a8
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-14 07:35:26 +0300

    Update practiceProblems.js

--- lesson4/practiceProblems.js
+++ lesson4/practiceProblems.js
@@ -1,3 +1,17 @@
 
+console.log([1, 2, 3].every(num => {
+    return num = num * 2;
+  }));
+// 2
+// 4
+// 6
+// true
+
+// console.log(['ant', 'bear', 'caterpillar'].pop().length); THis one was crazy because
+// console.log([1, 2, 3].map(num => num * num));
+
+// console.log([1, 2, 3].map(num => {
+//     return num * num;
+//   }));
 
 // console.log([1, 2, 3].filter(num => 'hi')); The value is truthy therefore it will just return all the values without evaluating them.

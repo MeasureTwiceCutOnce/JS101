350e6ba086a0acf39a952990efaaee8b44680f18
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-15 06:17:00 +0300

    Update practiceProblems.js

--- lesson4/practiceProblems.js
+++ lesson4/practiceProblems.js
@@ -1,3 +1,64 @@
+// Using a for loop. It is amazing how much you do not know.
+let statement = "The Flintstones Rock";
+
+let result = {};
+
+for (let counter = 0; counter < statement.length; counter += 1) {
+  let char = statement[counter];
+  if (char === ' ') continue;
+
+  result[char] = result[char] || 0;
+  result[char] += 1;
+}
+
+console.log(result)
+// let charsInStatement = statement.split("").filter(char => char !== " ");
+// let result = {};
+
+// charsInStatement.forEach(char => {
+// result[char] = result[char] || 0;
+// result[char] += 1;
+// })
+
+// console.log(result);
+// // or like this without the || operator
+
+// let charsInStatement = statement.split('').filter(char => char !== ' ');
+// let result = {};
+
+// charsInStatement.forEach(char => {
+//   if (Object.keys(result).includes(char)) {
+//     result[char] += 1;
+//   } else {
+//     result[char] = 1;
+//   }
+// });
+// let ages = {
+//     Herman: 32,
+//     Lily: 30,
+//     Grandpa: 5843,
+//     Eddie: 10,
+//     Marilyn: 22,
+//     Spot: 237
+//   };
+
+
+//   function smallestMember(obj) {
+
+// // let agesKeyValues = Object.entries(ages);
+// let agesValues = Object.values(obj);
+//     return Math.min(...agesValues);
+//     // let pointer1 = agesValues[0];
+
+//     // for (let i = 1; i < agesValues.length; i++) {
+//     //      if (pointer1 > agesValues[i]) {
+//     //         pointer1 = agesValues[i];
+//     //      }
+//     //      return pointer1;
+//     // }
+
+// }
+// console.log(smallestMember(ages))
 // Now that is an interesting things to say, "don't go method hunting" because that is exactly what I do everytime I have a problem. I search for a method that will do what I need done, and maybe that is a source of my confusion. I need to maybe stick to those few methods that I do know and make sure that I use those correctly everytime.
 
 // let ages = {

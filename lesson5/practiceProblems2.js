342f5f1d114a1da8d58cfa54bdc80781f0c8cd78
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-17 07:49:03 +0300

    Update practiceProblems2.js

--- lesson5/practiceProblems2.js
+++ lesson5/practiceProblems2.js
@@ -1,10 +1,24 @@
-let arr = ['10', '11', '9', '7', '8'];
-let numbers = [];
+let books = [
+    { title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967' },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925' },
+    { title: 'War and Peace', author: 'Leo Tolstoy', published: '1869' },
+    { title: 'Ulysses', author: 'James Joyce', published: '1922' },
+    { title: 'The Book of Kells', author: 'Multiple Authors', published: '800' },
+  ];
 
-arr.map(num => numbers.push(Number(num)));
-console.log(numbers);
+let sortedByYear = books.map(el => {
+    return Object.values(books)
+})
+
+console.log()
+
+// let arr = ['10', '11', '9', '7', '8'];
+// let numbers = [];
+
+// arr.map(num => numbers.push(Number(num)));
+// console.log(numbers);
 
 
-let sortedArray = numbers.sort((a,b) => b - a);
-console.log(sortedArray);
+// let sortedArray = numbers.sort((a,b) => b - a);
 // console.log(sortedArray);
+// // console.log(sortedArray);

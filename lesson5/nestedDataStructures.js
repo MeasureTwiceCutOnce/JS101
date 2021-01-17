ec390fa580750462f6a0fb767231935904b9fca1
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-17 06:54:32 +0300

    Update nestedDataStructures.js

--- lesson5/nestedDataStructures.js
+++ lesson5/nestedDataStructures.js
@@ -1,4 +1,14 @@
-let arr = [[1, 3], [2]];
-arr[0][1] = 5;
+let myArr = [[18, 7], [3, 12]].forEach(arr => {
+    return arr.map(num => {
+      if (num > 5) {
+        return console.log(num);
+      }
+    });
+  });
 
-console.log(arr);
+
+
+// let arr = [[1, 3], [2]];
+// arr[0][1] = 5;
+
+// console.log(arr);

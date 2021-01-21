d6f50da66b52242e49a3d69355e2367ff2292340
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-21 20:59:42 +0300

    Lesson 5 complete

---
+++ smallProblems/bannerizer.js
@@ -0,0 +1,18 @@
+// function logInBox(string) {
+//     let length = string.length;
+
+//     return (Array(5));
+// }
+
+
+// console.log(logInBox('To boldly go where no one has gone before.'));
+
+console.log(4);
+
+/*+--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+*/
+

7e73e6f3a8dbf33a06208c8e19ad800684e2bf16
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-22 20:03:45 +0300

    Update bannerizer.js

--- smallProblems/bannerizer.js
+++ smallProblems/bannerizer.js
@@ -20,4 +20,29 @@ console.log(logInBox(""));
 |                                            |
 +--------------------------------------------+
 */
+// THe code below is what I found online and I really like how they did this. It is very smooth with the variables very clear and the way that it is so short.
 
+// Write a method that will take a short line of text, and print it within a box.
+// const printInBox = str => {
+//     const length = str.length + 4;
+//     console.log(buildLine('+', '-', length));
+//     console.log(buildLine('|', ' ', length));
+//     console.log(formMessageLine(str, length));
+//     console.log(buildLine('|', ' ', length));
+//     console.log(buildLine('+', '-', length));
+// };
+
+// const buildLine = (border, fill, length) => {
+// let line = Array(length);
+// line.fill(fill);
+// return line.map((el, index) => (index === 0 || index === length - 1) ? border : el).join('');
+// }
+
+// const formMessageLine = (str, length) => {
+// let line = str.split('');
+// line.unshift('|', ' ');
+// line.push(' ', '|');
+// return line.join('');
+// };
+// //printInBox('To boldly go where no one has gone before.');
+// printInBox('Hello World!');

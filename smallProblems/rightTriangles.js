24980a336d28ebe05b846dbe80be36f09ae3989d
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-24 16:45:18 +0300

    Small Problems find Fibo

---
+++ smallProblems/rightTriangles.js
@@ -0,0 +1,53 @@
+// Write a function that takes a positive integer, n, as an argument, and logs a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+// function triangle(num) {
+    // let sub = 1;
+    // let targetLength = num + 1;
+//     let stars = "";
+
+//     while(targetLength >= 0) {
+//         stars + "*".padStart(targetLength - sub, " ");
+//         sub++;
+//     }
+//     console.log(stars)
+// }
+let string = "";
+
+console.log(string.concat("__*"));
+// function summation(number) {
+//     let sub = 1;
+//     let totalStars = number;
+//     let count = number;
+
+//     while(count > 0) {
+//         totalStars += number - sub;
+//         count--;
+//         sub++;
+// }
+//     return  totalStars;
+// }
+
+
+// triangle(5);
+// triangle(3);
+// triangle(4);
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+// triangle(9);
+
+//         *
+//        **
+//       ***
+//      ****
+//     *****
+//    ******
+//   *******
+//  ********
+// *********
+
+

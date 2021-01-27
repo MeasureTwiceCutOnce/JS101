28ec396daa4b9c753b04d7f56bae2687769e2e19
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-27 08:08:29 +0300

    Create searching101.js

---
+++ easy4/searching101.js
@@ -0,0 +1,35 @@
+
+
+function isNumFound(numbers) {
+    let numbersArray =
+
+    console.log(numbersArray);
+}
+
+function getUserInput(message) {
+
+    return numbers;
+}
+
+isNumFound(userInput);
+// Write a program that solicits six numbers from the user, then logs a message that describes whether or not the sixth number appears amongst the first five numbers.
+
+// Enter the 1st number: 25
+// Enter the 2nd number: 15
+// Enter the 3rd number: 20
+// Enter the 4th number: 17
+// Enter the 5th number: 23
+// Enter the last number: 17
+
+// The number 17 appears in 25,15,20,17,23.
+
+// -----
+
+// Enter the 1st number: 25
+// Enter the 2nd number: 15
+// Enter the 3rd number: 20
+// Enter the 4th number: 17
+// Enter the 5th number: 23
+// Enter the last number: 18
+
+// The number 18 does not appear in 25,15,20,17,23.

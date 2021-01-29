cb8c9b55d6bb956878a90a5e8d85219fda14b207
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-29 20:00:04 +0300

    Create runningTotals.js

---
+++ smallProblems/easy4/runningTotals.js
@@ -0,0 +1,63 @@
+// function runningTotal(array) {
+//     let newArray = [];
+//     let total = 0;
+
+//     for (let i = 0; i < array.length; i++) {
+//         newArray.push(total += array[i]);
+//     }
+//     return newArray;
+// }
+// function runningTotal(array) {
+//     let newArray = [];
+//     let total = 0;
+
+//     array.map((a,b) => {
+//         total = a + b;
+//         newArray.push(total),array[0])
+
+//         return newArray;
+//     }
+// }
+
+// Above is my attempt.
+// function runningTotal(array) {
+//     if (array.length === 0) return array;
+//     let result = [array[0]];
+//     for (let idx = 1; idx < array.length; idx += 1) {
+//       result.push(array[idx] + result[idx - 1]);
+//     }
+//     return result;
+//   }
+// The above is Boris' answer and it looks like what I was trying to do.
+
+
+// The following is Wes Anderson's answer which look pretty good. I like how he used the reduce method.
+
+// function runningTotal(array) {
+//     return array.reduce((acc, ele, idx) => {
+//         if (idx > 0) {
+//             acc.push(ele + acc[idx - 1])
+//         } else {
+//             acc.push(ele)
+//         }
+//         return acc;
+//     }, []);
+// }
+
+// The below isCoen's answer
+function runningTotal(arr) {
+    let sum = 0;
+
+    let result = arr.map((num, idx) => {
+      return sum += num;
+    });
+
+    return result;
+  }
+
+console.log(
+runningTotal([2, 5, 13]),             // [2, 7, 20]
+runningTotal([14, 11, 7, 15, 20]),    // [14, 25, 32, 47, 67]
+runningTotal([3]),                    // [3]
+runningTotal([]),                     // []
+)

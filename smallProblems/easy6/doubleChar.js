29805d195ad358381c483addd5007b4eaa3a98a6
Rafiq Kamal <71498824+rafiq@users.noreply.github.com>
2021-02-04 09:29:17 +0300

    Create doubleChar.js

---
+++ smallProblems/easy6/doubleChar.js
@@ -0,0 +1,11 @@
+function repeater(string) {
+
+    let doubled = string.split("").map(char => char + char,[])
+
+    return doubled.join("");
+}
+console.log(
+repeater('Hello'),        // "HHeelllloo"
+repeater('Good job!'),    // "GGoooodd  jjoobb!!"
+repeater(''),             // ""
+)

3f87f65a9af6c4407dbc3d9ff34bf3e0c26431ab
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-02-14 09:27:16 +0300

    Update bubbleSort.js

--- smallProblems/Med2/bubbleSort.js
+++ smallProblems/Med2/bubbleSort.js
@@ -1,7 +1,19 @@
+// My function did work however it needed a double for loop to work while I originally had a for loop and a while loop on the inside. I am sure it could have worked if I got the syntax correct. I am a little confused on why the double for loop is not calling the i variable anywhere. OHHH! I think I get it. The i loop is checking the entire array while the j loop is checking the adjacent pair only. So the i loop will keep running until every j pair is correct.
+
 function bubbleSort(array) {
+    let temp = 0;
 
-
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
     return array;
+
 }
 
 

f11fd154119d6b9ee6bf21d4617c77fa5658cbb4
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-02-17 17:05:44 +0300

    Update diamonds.js

--- smallProblems/interpretiveProblemSolving/diamonds.js
+++ smallProblems/interpretiveProblemSolving/diamonds.js
@@ -3,6 +3,38 @@ function diamond(num) {
     let center = Math.ceil(num / 2);
     let padding = center;
 
+    // let row = num;
+    // let height = num;
+    let stars = "*";
+    let up = 1;
+    while (num > 0) {
+
+        for (let height = 0; height < num * 2; height++) {
+            console.log((stars.repeat(up)).padStart(num ," "))
+            up++;
+            num--;
+        }
+        for (let height = 0; height < num * 2; height++) {
+            console.log((stars.repeat(num)).padStart(num + 2," "))
+            num--;
+        }
+    }
+    // return stars;
+
+    // return stars;
+    // The below code worked but I got it from a source on line and, I forgot about the repeat method. so I am going to try and do this without looking at the rest of the answer.
+    // var row, width, shape = '';
+
+    // for(row = 0; row < num * 2 - 1; row++) {
+    //   width = row < num ? row : num * 2 - row - 2;
+    //   shape += Array(num - width).join(' ') + Array(width + 1).join('* ') + '*\n';
+    // }
+    // console.log(shape)
+///////////////////////////////////////////***************************** */
+
+
+
+
     // for (let row = 0; row < num; row++) {
     //     console.log(diamondString.padStart(padding,"-"))
     //     diamondString += "**"
@@ -14,13 +46,6 @@ function diamond(num) {
     //     diamondString = row >=num ? diamondString.slice(3) :diamondString;
     // }
     // return diamondString
-    var row, width, shape = '';
-
-    for(row = 0; row < num * 2 - 1; row++) {
-      width = row < num ? row : num * 2 - row - 2;
-      shape += Array(num - width).join(' ') + Array(width + 1).join('* ') + '*\n';
-    }
-    console.log(shape)
     // if (row === 1) {
     //     console.log(shape);
     // } else if (row && 1) {
@@ -33,8 +58,8 @@ function diamond(num) {
 //     console.log(value);
 // }
 console.log(
-diamond(1),// logs *
-diamond(9),
+// diamond(1),// logs *
+// diamond(9),
 diamond(3),
 /* logs
     *

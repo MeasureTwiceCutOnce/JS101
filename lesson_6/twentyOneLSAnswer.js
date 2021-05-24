df1af56605e55e649d9159af1ad33b5f681eb1dc
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-24 21:43:42 +0300

    Update twentyOneLSAnswer.js

--- lesson_6/twentyOneLSAnswer.js
+++ lesson_6/twentyOneLSAnswer.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync');
 
 const SUITS = ['H', 'D', 'S', 'C'];
-const VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const VALUES = ['1','2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
 
 function prompt(message) {
   console.log(`=> ${message}`);
